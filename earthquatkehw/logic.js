73712396b86d8175ba6d39cae33656a0156b8a44
jmekblad13 <33137283+jmekblad13@users.noreply.github.com>
2018-06-24 20:22:42 -0500

    Add files via upload

--- earthquatkehw/logic.js
+++ earthquatkehw/logic.js
@@ -162,8 +162,8 @@ function createMap(earthquakes) {
 
   // Create overlay object to hold our overlay layer
   var overlayMaps = {
-    Earthquakes: earthquakes,
-    Fault_Lines: faults
+    Earthquakes: earthquakes
+    //Fault_Lines: faults
   };
 
   // Create our map, giving it the streetmap and earthquakes layers to display on load
